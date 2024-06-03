88e4b390a8668d430ad9339a1b457f9503fc4c3c
Christian Colberg <kroll94@gmail.com>
2024-06-03 12:45:31 +0200

    Initial commit

---
+++ CRUD-SQL Assignment June 2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_SQL_Assignment_June_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
